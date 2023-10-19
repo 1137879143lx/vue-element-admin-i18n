b9fc3c71adcb74d239ba8a03ed0f969638c78acb
1137879143@QQ.COM <1137879143@QQ.COM>
2023-10-19 11:45:03 +0800

    删除部分包

--- src/main.js
+++ src/main.js
@@ -14,7 +14,6 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
-import PrimeVue from 'primevue/config'
 
 import i18n from './lang' // internationalization
 import './icons' // icon
@@ -35,7 +34,6 @@ if (process.env.NODE_ENV === 'production') {
   const { mockXHR } = require('../mock')
   mockXHR()
 }
-Vue.use(PrimeVue)
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
