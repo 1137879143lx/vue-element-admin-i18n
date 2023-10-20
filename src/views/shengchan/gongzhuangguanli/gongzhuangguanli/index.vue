867673e5bb5f744b75399851699ea5f522909b36
1137879143@QQ.COM <1137879143@QQ.COM>
2023-10-20 11:06:42 +0800

    更新进度

---
+++ src/views/shengchan/gongzhuangguanli/gongzhuangguanli/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="gongzhuangguanli" />
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+export default {
+  name: 'Gongzhuangguanli',
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
