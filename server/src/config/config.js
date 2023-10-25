bd812004a4c6bcdae6c36b9ed765a201be323792
1137879143@QQ.COM <1137879143@QQ.COM>
2023-10-25 23:25:21 +0800

    更新

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,5 @@
 var config = {
-  database: 'mongodb://localhost:27017/LiuXu_mes', // 使用哪个数据库
+  database: 'mongodb://localhost:27017/liuxu_mes', // 使用哪个数据库
   // 加密和解密 Token 的密钥
   jwtSecretKey: 'ai xiao su ^_^',
   // token 的有效期
